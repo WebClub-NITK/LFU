3ecc3753b9404b071526ba5e46551fe435651457
Mohit Reddy <mohitreddy1996@users.noreply.github.com>
2017-05-02 23:47:53 +0530

    C++ (#5)
    * First Cut made
    
    * Insert done. Refractoring to be done
    
    * Remove implemented
    
    * Access code done.
    
    * Testing skeleton code added
    
    * Code restart
    
    * LFU Testing done!
    
    * Todos Removed
    
    * Refractoring
    
    * Changes and Cpp Unit Tests Added.
    
    * Source files added
    
    * Cmake files removed

---
+++ header/LfuTest.h
@@ -0,0 +1,38 @@
+//
+// Created by mohit on 20/2/17.
+//
+
+#ifndef LFU_LFUTEST_H
+#define LFU_LFUTEST_H
+
+#include "cppunit/TestFixture.h"
+#include "cppunit/TestSuite.h"
+#include "cppunit/TestResult.h"
+#include "cppunit/TestCaller.h"
+#include "cppunit/TestAssert.h"
+#include "cppunit/ui/text/TestRunner.h"
+
+#include "Lfu.h"
+
+class LfuTest : public CppUnit::TestFixture {
+private:
+    Lfu *lfu;
+public:
+    LfuTest();
+    ~LfuTest();
+    void AddValue(int value);
+    void LookUpValue(int value);
+    void Evict(int value);
+    void EvictFromCache();
+    void PrintLfu();
+    void setUp();
+    void tearDown();
+    void testAdd();
+    void testAdd1();
+    void testAdd2();
+    void testAdd3();
+    CppUnit::TestSuite *suite();
+};
+
+
+#endif //LFU_LFUTEST_H
